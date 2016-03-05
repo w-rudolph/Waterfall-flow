14b188522237d4d6bd3b6fa6693cb6ca1bc39809
172247097@qq.com <172247097@qq.com>
2016-03-05 22:17:49 +0800

    Upload files

---
+++ Waterfall-flow.js
@@ -0,0 +1,34 @@
+﻿(function($){
+ $.fn.extend({
+   waterfallflow: function(options){
+	 var defaults = {
+		col: 3,
+		target: 'div'
+     },
+     options = $.extend(defaults, options);
+	 return this.each(function (){
+	   var $this = $(this),
+		   $items = $this.children(options.target),
+		   count = $items.length,
+		   count_row = Math.ceil(count/options.col),
+		   i = j = 0; /*i: num of row, j: num of col*/
+		   $items.each(function(index){
+			var $this = $(this);
+			if(j == options.col){
+			 i++;
+			 j %= options.col;
+			}
+			$this.css({top: '0px', left: '0px'});
+			var top_index = (i == 0 ? 0 : i - 1) * options.col + j,
+				$top_item = $items.eq(top_index),
+				pre_index = i * options.col + (j == 0 ? 0 : j - 1),
+				$pre_item = $items.eq(pre_index),
+				top = $top_item.position().top+( i==0 ? 0 : 1 ) * $top_item.innerHeight()+'px',
+				left = $pre_item.position().left+( j==0 ? 0 : 1 )* $pre_item.innerWidth()+'px';	 
+			$this.css({top: top, left: left});
+			j++;
+		 });	
+	 });	 
+   },
+ })	
+})(jQuery);
